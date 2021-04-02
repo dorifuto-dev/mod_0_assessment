432aa1277f45ba75a84f93b530f1589c703c9a9b
Eric Li <ericli@knights.ucf.edu>
2021-04-02 12:18:50 -0600

    Add dinosaur to modelA.js

---
+++ models/modelA.js
@@ -0,0 +1,10 @@
+var dinosaurOne = {
+  genus: "Tyrannosaurus",
+  species: "Rex",
+  carnivore: true,
+  extinctStatus: true,
+  heightInFeet: 20,
+  weightInLbs: 15000,
+  region: ["North America", "Asia"],
+  favoriteFood: ["Edmontosaurus", "Triceratops"]
+}
