25f77f1c34e8727c53ac44fb26948029998fcf9d
Eric Li <ericli@knights.ucf.edu>
2021-04-02 12:25:37 -0600

    Add developer to modelB.js

---
+++ models/modelB.js
@@ -0,0 +1,10 @@
+var ericTheDeveloper = {
+  firstName: "Eric",
+  lastName: "Li",
+  ageInYears: 24,
+  numberOfFingers: 10,
+  enrolledInTuring: true,
+  hasTransportation: true,
+  favoriteLanguages: ["Python", "Javascript", "HTML"],
+  favoriteDrink: ["Sweet Tea", "Orange Juice", "Old Fashioned"]
+}
